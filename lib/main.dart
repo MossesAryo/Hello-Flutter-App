8198e16402b3d3f2991e67aaecf1fdae93fa5d12
MossesAryo <mossesaryobimo@gmail.com>
2025-01-09 20:54:50 +0700

    Hot Reload,Custom Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text("Hello Flutter App!"),),
-      body: Text("Hello Flutter"),
-    ),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Flutter App!"),
+        ),
+        body: Text("Hello Flutter"),
+      ),
+    );
+  }
 }
