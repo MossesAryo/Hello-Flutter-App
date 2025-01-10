5f45412733202d9649c2aa368adf6aab7e2c9975
MossesAryo <mossesaryobimo@gmail.com>
2025-01-10 19:25:46 +0700

    using column inside the container

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,19 @@ class MyApp extends StatelessWidget {
           title: Text("Hello Flutter"),
         ),
         body: Container(
-          padding: EdgeInsets.all(10),
-          margin: EdgeInsets.all(10),
-          child: Text("Hello Flutter"),
-          height: 100,
-          width: 100,
-          decoration: BoxDecoration(
-              color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+          padding: EdgeInsets.all(20),
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(color: Colors.blue[100]),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("1st children"),
+              Text("2nd children"),
+              Text("3rd children"),
+            ],
+          ),
         ),
       ),
     );
